4463a8d8c4b4696f07632293c8d207f108126472
Alex Filip <alexfilippaul@gmail.com>
2022-11-23 21:59:49 +0200

    revizuit

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -6,7 +6,27 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello everybody:");
+            int width;
+            int length;
+
+            //Console.WriteLine("Acest program va calcula aria unuei camere");
+            Console.WriteLine("Enter width: ");
+            width = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter length: ");
+            length = int.Parse(Console.ReadLine());
+
+           /* int ariaCamerei = latime * lungime;
+            Console.WriteLine("Aria camerei este " + ariaCamerei);*/
+
+            if(width == length)
+                Console.WriteLine("Width and Length are equal");
+
+            else if (width < length)
+                    Console.WriteLine("Width is smaller than length");
+            else if (width > length)
+                    Console.WriteLine("Width is greater than length");
+
 
         }
     }
